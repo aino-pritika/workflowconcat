806163b7b5cd67eab95a47a670e5af37244afc13
ritika padwal <ainosoft1@ainosoft1-System-Product-Name>
2022-05-03 11:10:34 +0530

    previewengine and wf6 files are added

---
+++ Workflow/wf6.js
@@ -0,0 +1,26 @@
+
+//   // var firstname = $("#txt1").val();
+//   // var lastname = $("#txt1").val();
+
+
+
+var wfs={
+    "action":
+    {
+        "name":"updatelabel",
+        "on":".btn-action"
+    }    ,
+   
+    "todo":{
+      "bt.text":"firstname.text+lastname.text"
+    
+}
+}
+
+
+// console.log(firstname+lastname);
+// console.log(wfs.todo["bt1.txt"]);
+  
+  
+//console.log(wfs.todo["bt1.txt"]);
+//console.log(str1);
